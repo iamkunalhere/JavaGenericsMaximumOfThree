cd365094a998b615261ec4e2e4cc0eca615e61b9
iamkunalhere <iamkunalhere@gmail.com>
2020-04-01 00:10:59 +0530

    Welcome to Find Maximum Problem using Java Generics

---
+++ src/main/java/MaximumOfThree.java
@@ -0,0 +1,16 @@
+public class MaximumOfThree {
+    public static Integer maxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maxNumber = null;
+        Integer True = 1;
+        if (firstNumber.compareTo(secondNumber) == True && firstNumber.compareTo(thirdNumber) == True) {
+            maxNumber=firstNumber;
+        }
+        else if (secondNumber.compareTo(firstNumber) == True && secondNumber.compareTo(thirdNumber) == True) {
+            maxNumber=secondNumber;
+        }
+        else if (thirdNumber.compareTo(firstNumber) == True && thirdNumber.compareTo(secondNumber) == True) {
+            maxNumber=thirdNumber;
+        }
+        return maxNumber;
+    }
+}
