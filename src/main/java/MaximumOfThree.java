ce299484912013d1cb8c3b28647d2dafb6e6b62c
iamkunalhere <iamkunalhere@gmail.com>
2020-04-01 01:10:32 +0530

    ADDED: UC2 find maximum from three floats

--- src/main/java/MaximumOfThree.java
+++ src/main/java/MaximumOfThree.java
@@ -1,9 +1,23 @@
 public class MaximumOfThree {
 
+    public static int True = 1;
+    // Method for finding maximum of three integers
     public static Integer maxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-
         Integer maxNumber = null;
-        Integer True = 1;
+        if (firstNumber.compareTo(secondNumber) == True && firstNumber.compareTo(thirdNumber) == True ) {
+            maxNumber=firstNumber;
+        }
+        else if (secondNumber.compareTo(firstNumber) == True && secondNumber.compareTo(thirdNumber) == True) {
+            maxNumber=secondNumber;
+        }
+        else if (thirdNumber.compareTo(firstNumber) == True && thirdNumber.compareTo(secondNumber) == True) {
+            maxNumber=thirdNumber;
+        }
+        return maxNumber;
+    }
+    // Method for finding maximum of three floats
+    public static Float maxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float maxNumber = null;
         if (firstNumber.compareTo(secondNumber) == True && firstNumber.compareTo(thirdNumber) == True) {
             maxNumber=firstNumber;
         }
