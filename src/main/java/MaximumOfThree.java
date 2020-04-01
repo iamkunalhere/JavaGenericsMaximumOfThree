1cce73d6de05b6eea2e2f5f76625c6ce533647e5
iamkunalhere <iamkunalhere@gmail.com>
2020-04-02 00:04:27 +0530

    REFACTORED: All methods of finding maximum interger,float and string to one Generic method

--- src/main/java/MaximumOfThree.java
+++ src/main/java/MaximumOfThree.java
@@ -1,46 +1,16 @@
-public class MaximumOfThree {
-
-    public static int True = 1;
-    // Method for finding maximum of three integers
-    public static Integer maxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer maxNumber = null;
-        if (firstNumber.compareTo(secondNumber) == True && firstNumber.compareTo(thirdNumber) == True ) {
-            maxNumber=firstNumber;
+public class  MaximumOfThree {
+    public static <E extends Comparable<E>> E findMaximum(E firstArgument, E secondArgument, E thirdArgument) {
+        E max = null;
+        int True = 1;
+        if (firstArgument.compareTo(secondArgument) == True && firstArgument.compareTo(thirdArgument) == True) {
+            max=firstArgument;
         }
-        else if (secondNumber.compareTo(firstNumber) == True && secondNumber.compareTo(thirdNumber) == True) {
-            maxNumber=secondNumber;
+        else if (secondArgument.compareTo(firstArgument) == True && secondArgument.compareTo(thirdArgument) == True) {
+            max=secondArgument;
         }
-        else if (thirdNumber.compareTo(firstNumber) == True && thirdNumber.compareTo(secondNumber) == True) {
-            maxNumber=thirdNumber;
+        else if (thirdArgument.compareTo(firstArgument) == True && thirdArgument.compareTo(secondArgument) == True){
+            max=thirdArgument;
         }
-        return maxNumber;
-    }
-    // Method for finding maximum of three floats
-    public static Float maxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        Float maxNumber = null;
-        if (firstNumber.compareTo(secondNumber) == True && firstNumber.compareTo(thirdNumber) == True) {
-            maxNumber=firstNumber;
-        }
-        else if (secondNumber.compareTo(firstNumber) == True && secondNumber.compareTo(thirdNumber) == True) {
-            maxNumber=secondNumber;
-        }
-        else if (thirdNumber.compareTo(firstNumber) == True && thirdNumber.compareTo(secondNumber) == True) {
-            maxNumber=thirdNumber;
-        }
-        return maxNumber;
-    }
-    // Method for finding maximum of three strings
-    public static String maxString(String firstString, String secondString, String thirdString) {
-        String maxString = null;
-        if (firstString.length() > secondString.length() && firstString.length() > thirdString.length()) {
-            maxString=firstString;
-        }
-        else if (secondString.length() > firstString.length() && secondString.length() > thirdString.length()) {
-            maxString=secondString;
-        }
-        else if (thirdString.length() > firstString.length() && thirdString.length() > secondString.length()) {
-            maxString=thirdString;
-        }
-        return maxString;
+        return max;
     }
 }
--- src/test/java/MaximumOfThreeTest.java
+++ src/test/java/MaximumOfThreeTest.java
@@ -8,55 +8,55 @@ public class MaximumOfThreeTest {
     // Test for if first integer is max
     @Test
     public void givenInput_whenFirstNumberIsMaximum_shouldReturnMaximum() {
-        int firstMaximum = maximumOfThreeObject.maxInteger(3,2,1);
-        Assert.assertEquals(3,firstMaximum);
+        Integer firstMaximum = (Integer) maximumOfThreeObject.findMaximum(3,2,1);
+        Assert.assertEquals( Integer.valueOf(3),firstMaximum);
     }
     // Test for if second integer is max
     @Test
     public void givenInput_whenSecondNumberIsMaximum_shouldReturnMaximum() {
-        int secondMaximum = maximumOfThreeObject.maxInteger(1,3,2);
-        Assert.assertEquals(3,secondMaximum);
+        Integer secondMaximum = maximumOfThreeObject.findMaximum(1,3,2);
+        Assert.assertEquals(Integer.valueOf(3),secondMaximum);
     }
     // Test for if third integer is max
     @Test
     public void givenInput_whenThirdNumberIsMaximum_shouldReturnMaximum() {
-        int thirdMaximum = maximumOfThreeObject.maxInteger(1,2,3);
-        Assert.assertEquals(3,thirdMaximum);
+        Integer thirdMaximum = maximumOfThreeObject.findMaximum(1,2,3);
+        Assert.assertEquals(Integer.valueOf(3),thirdMaximum);
     }
     // Test for if first float is max
     @Test
     public void givenInput_whenFirstFloatIsMaximum_shouldReturnMaximum() {
-        Float firstMaximum = maximumOfThreeObject.maxFloat(3.3f,2.2f,1.1f);
-        Assert.assertEquals((Float)3.3f,firstMaximum);
+        Float firstMaximum = maximumOfThreeObject.findMaximum(3.3f,2.2f,1.1f);
+        Assert.assertEquals(Float.valueOf(3.3f),firstMaximum);
     }
     // Test for if second float is max
     @Test
     public void givenInput_whenSecondFloatIsMaximum_shouldReturnMaximum() {
-        Float secondMaximum = maximumOfThreeObject.maxFloat(1.1f,3.3f,2.2f);
-        Assert.assertEquals((Float) 3.3f,secondMaximum);
+        Float secondMaximum = maximumOfThreeObject.findMaximum(1.1f,3.3f,2.2f);
+        Assert.assertEquals(Float.valueOf(3.3f),secondMaximum);
     }
     // Test for if third float is max
     @Test
     public void givenInput_whenThirdFloatIsMaximum_shouldReturnMaximum() {
-        Float thirdMaximum = maximumOfThreeObject.maxFloat(1.1f,2.2f,3.3f);
-        Assert.assertEquals((Float)3.3f,thirdMaximum);
+        Float thirdMaximum = maximumOfThreeObject.findMaximum(1.1f,2.2f,3.3f);
+        Assert.assertEquals(Float.valueOf(3.3f),thirdMaximum);
     }
     // Test for if first string is max
     @Test
     public void givenInput_whenFirstStringIsMaximum_shouldReturnMaximum() {
-        String firstMaximum = maximumOfThreeObject.maxString("banana","apple","peach");
+        String firstMaximum = maximumOfThreeObject.findMaximum("banana","apple","asdf");
         Assert.assertEquals("banana",firstMaximum);
     }
     // Test for if second string is max
     @Test
     public void givenInput_whenSecondStringIsMaximum_shouldReturnMaximum() {
-        String secondMaximum = maximumOfThreeObject.maxString("apple","banana","peach");
+        String secondMaximum = maximumOfThreeObject.findMaximum("apple","banana","asdf");
         Assert.assertEquals("banana",secondMaximum);
     }
     // Test for if third string is max
     @Test
     public void givenInput_whenThirdStringIsMaximum_shouldReturnMaximum() {
-        String thirdMaximum = maximumOfThreeObject.maxString("apple","peach","banana");
+        String thirdMaximum = maximumOfThreeObject.findMaximum("apple","asdf","banana");
         Assert.assertEquals("banana",thirdMaximum);
     }
 }
