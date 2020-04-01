818392bbb0e73ed1656311484b7e943c8c061c3e
iamkunalhere <iamkunalhere@gmail.com>
2020-04-02 01:52:07 +0530

    ADDED: UC5 for printing maximum from multiple arguments

--- src/main/java/MaximumOfThree.java
+++ src/main/java/MaximumOfThree.java
@@ -1,14 +1,22 @@
 import java.util.Arrays;
 
 public class  MaximumOfThree <E extends Comparable<E>>{
-
+    // Array arguments of type E
     E[] arguments;
-
+    // Constructor for initialization of array elements
     public MaximumOfThree(E[] array) {
         this.arguments=array;
     }
+    // method to find maximum in given array
     public static <E extends Comparable<E>> E findMaximum(E... arguments) {
         Arrays.sort(arguments);
+        E maximum = arguments[arguments.length-1];
+        printMax(maximum);
         return arguments[arguments.length-1];
     }
+    // method to print maximum from given array
+    private static <E extends Comparable<E>> void printMax(E maximum) {
+        System.out.println("Maximum is "+maximum);
+    }
+
 }
